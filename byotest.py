007fd41c92286617451884234fd5ce06ffa72d39
Ubuntu <ubuntu@ip-172-31-39-19.ec2.internal>
2019-10-26 14:18:34 +0000

    forgot to add the is_equal function

--- byotest.py
+++ byotest.py
@@ -1,3 +1,6 @@
+def test_equal(a,b):
+    assert a==b, "{0} and {1} aren't equal".format(a,b)
+    
 def test_not_equal(a,b):
     assert a!=b, "Did not expect {0} but got {1}".format(a,b)
 
@@ -12,13 +15,16 @@ def test_between(value,low, high):
 
 
 # Test to fail the test_not_equal function
-# test_not_equal(2,2)
+test_equal(1,2)
+
+# Test to fail the test_not_equal function
+test_not_equal(2,2)
 
 # Test to fail the test_is_in function
-# test_is_in([1,2,3],5)
+test_is_in([1,2,3],5)
 
 # Test to fail the test_not_in function
-# test_not_in([1,2,3,5],5)
+test_not_in([1,2,3,5],5)
 
 # Test to fail the test_between function
 test_between(7,3,6)
