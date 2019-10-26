500733be23104ccbb393685765da85411ad5f09b
Ubuntu <ubuntu@ip-172-31-39-19.ec2.internal>
2019-10-26 14:12:17 +0000

    finished writing test from challenges into byotest.py

---
+++ byotest.py
@@ -0,0 +1,24 @@
+def test_not_equal(a,b):
+    assert a!=b, "Did not expect {0} but got {1}".format(a,b)
+
+def test_is_in(collection,item):
+    assert item in collection, "{0} does not contain {1}".format(collection,item)
+
+def test_not_in(collection,item):
+    assert item not in collection, "{0} does contain {1}".format(collection,item)
+
+def test_between(value,low, high):
+    assert value in range(low,high+1),"{0} is not between {1} and {2}".format(value, low, high)
+
+
+# Test to fail the test_not_equal function
+# test_not_equal(2,2)
+
+# Test to fail the test_is_in function
+# test_is_in([1,2,3],5)
+
+# Test to fail the test_not_in function
+# test_not_in([1,2,3,5],5)
+
+# Test to fail the test_between function
+test_between(7,3,6)
